c2b3675311917f4bbe0bd55e7c22fc87bcde40b6
chengtao <icesxrun@gmail.com>
2021-04-08 09:16:59 +0800

    88%

--- the_captcha/yzm.py
+++ the_captcha/yzm.py
@@ -153,13 +153,13 @@ def train():
                   metrics=['accuracy'],
                   loss='categorical_crossentropy')
     model.summary()
-    batch_x, batch_y = get_next_batch(40000)
+    batch_x, batch_y = get_next_batch(50000)
     print(' batch_x.shape=', batch_x.shape, ' batch_y.shape=', batch_y.shape)
     batch_x_val, batch_y_val = get_next_batch(100)
     model.fit(batch_x, batch_y,
               batch_size=40,
-              epochs=120,
-              steps_per_epoch=1000,
+              epochs=100,
+              steps_per_epoch=1250,
               validation_data=(batch_x_val, batch_y_val))
     print("y预测=\n", np.argmax(model.predict(batch_x), axis=2))
     print("y实际=\n", np.argmax(batch_y, axis=2))
