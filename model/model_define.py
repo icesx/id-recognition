1d1b48d3ce4c8d9ddf42314ee9696fa50b6fd330
chengtao <chengtao@ithink>
2022-02-09 09:09:02 +0800

    del some file

--- model/model_define.py
+++ model/model_define.py
@@ -71,29 +71,29 @@ class ModelDefine:
 
 
 class IdModelDefine(ModelDefine):
-    IMAGE_HEIGHT = 32
-    IMAGE_WIDTH = 256
-    IMAGE_CHANNELS = 3
-    CHAR_SET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    CHAR_SET_LEN = len(CHAR_SET)
-    LABEL_LEN = 18
-    LABEL_SPACE_LEN = 10
-    DATASET_PATH = "/OTHER/dataset/id_card/"
+    __IMAGE_HEIGHT = 32
+    __IMAGE_WIDTH = 256
+    __IMAGE_CHANNELS = 3
+    __CHAR_SET = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    __CHAR_SET_LEN = len(__CHAR_SET)
+    __LABEL_LEN = 18
+    __LABEL_SPACE_LEN = 10
+    __DATASET_PATH = "/OTHER/dataset/id_card/"
 
     def image_height(self):
-        return self.IMAGE_HEIGHT
+        return self.__IMAGE_HEIGHT
 
     def image_width(self):
-        return self.IMAGE_WIDTH
+        return self.__IMAGE_WIDTH
 
     def train_path(self):
-        return self.DATASET_PATH + "train/"
+        return self.__DATASET_PATH + "train/"
 
     def val_path(self):
-        return self.DATASET_PATH + "val/"
+        return self.__DATASET_PATH + "val/"
 
     def test_path(self):
-        return self.DATASET_PATH + "test/"
+        return self.__DATASET_PATH + "test/"
 
     def steps_per_epoch(self):
         return 100
@@ -111,19 +111,19 @@ class IdModelDefine(ModelDefine):
         return "id"
 
     def image_channels(self):
-        return self.IMAGE_CHANNELS
+        return self.__IMAGE_CHANNELS
 
     def label_len(self):
-        return self.LABEL_LEN
+        return self.__LABEL_LEN
 
     def charset_len(self):
-        return self.CHAR_SET_LEN
+        return self.__CHAR_SET_LEN
 
     def label_space_len(self):
-        return self.LABEL_SPACE_LEN
+        return self.__LABEL_SPACE_LEN
 
     def charset(self):
-        return self.CHAR_SET
+        return self.__CHAR_SET
 
     def char_list_vector(self, label_str):
         return [np.array(to_categorical(int(i), self.label_space_len())) for i in list(label_str)]
@@ -133,34 +133,34 @@ class IdModelDefine(ModelDefine):
 
 
 class CaptchaModelDefine(ModelDefine):
-    choices = [
+    __CHAR_SET_OR = [
         list(string.digits),
         list(string.ascii_lowercase),
         list(string.ascii_uppercase),
     ]
-    IMAGE_CHANNELS = 3
-    CHAR_SET = sum(choices, [])
-    CHAR_SET_LEN = len(CHAR_SET)
-    LABEL_LEN = 4
-    LABEL_SPACE_LEN = 256
-    IMAGE_WIDTH = 40 + 20 * LABEL_LEN
-    IMAGE_HEIGHT = 100
-    DATASET_PATH = "/OTHER/dataset/captcha/"
+    __IMAGE_CHANNELS = 3
+    __CHAR_SET = sum(__CHAR_SET_OR, [])
+    __CHAR_SET_LEN = len(__CHAR_SET)
+    __LABEL_LEN = 4
+    __LABEL_SPACE_LEN = 256
+    __IMAGE_WIDTH = 40 + 20 * __LABEL_LEN
+    __IMAGE_HEIGHT = 100
+    __DATASET_PATH = "/OTHER/dataset/captcha/"
 
     def image_height(self):
-        return self.IMAGE_HEIGHT
+        return self.__IMAGE_HEIGHT
 
     def image_width(self):
-        return self.IMAGE_WIDTH
+        return self.__IMAGE_WIDTH
 
     def train_path(self):
-        return self.DATASET_PATH + "train/"
+        return self.__DATASET_PATH + "train/"
 
     def val_path(self):
-        return self.DATASET_PATH + "val/"
+        return self.__DATASET_PATH + "val/"
 
     def test_path(self):
-        return self.DATASET_PATH + "test/"
+        return self.__DATASET_PATH + "test/"
 
     def steps_per_epoch(self):
         return 100
@@ -178,19 +178,19 @@ class CaptchaModelDefine(ModelDefine):
         return "captcha"
 
     def image_channels(self):
-        return self.IMAGE_CHANNELS
+        return self.__IMAGE_CHANNELS
 
     def label_len(self):
-        return self.LABEL_LEN
+        return self.__LABEL_LEN
 
     def charset_len(self):
-        return self.CHAR_SET_LEN
+        return self.__CHAR_SET_LEN
 
     def label_space_len(self):
-        return self.LABEL_SPACE_LEN
+        return self.__LABEL_SPACE_LEN
 
     def charset(self):
-        return self.CHAR_SET
+        return self.__CHAR_SET
 
     def char_list_vector(self, label_str):
         return [np.array(to_categorical(ord(i), self.label_space_len())) for i in list(label_str)]
--- model_predict.py
+++ model_predict.py
@@ -5,12 +5,13 @@
 import tensorflow as tf
 from tensorflow import keras
 
-from define import IMAGE_WIDTH, IMAGE_HEIGHT, DATASET_PATH
+from model.model_define import IdModelDefine
 from model.predict import predict
 
 if __name__ == '__main__':
-    export_dir = "./save/model"
+    md = IdModelDefine()
+    export_dir = md.save_path()
     model = keras.models.load_model(export_dir)
-    label_predict = predict(model, DATASET_PATH + "test" + "/990196488967963319.png", IMAGE_HEIGHT, IMAGE_WIDTH)
+    label_predict = predict(model, md.test_path() + "/990196488967963319.png", md)
     label_predict = tf.math.argmax(label_predict, axis=-1)
     print(str(label_predict))
